7b29afa665e23d7d5534b4acc142730556a2d71b
Ralston Da Silva <ralstondasilva@gmail.com>
2016-06-16 11:45:02 -0700

    Add refresh button

--- Quakitivity/Quakitivity/ViewModel/QuakeActivityViewModel.cs
+++ Quakitivity/Quakitivity/ViewModel/QuakeActivityViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Quakitivity.Model;
+﻿using Quakitivity.Helpers;
+using Quakitivity.Model;
 using Quakitivity.NetworkCommunication;
 using Quakitivity.Properties;
 using System;
@@ -17,7 +18,7 @@ namespace Quakitivity.ViewModel
         //City margao = new City("Margao", "India");
         //City panjim = new City("Panjim", "India");
         //City vasco = new City("Vasco", "India");
-
+        public RelayCommand RefreshQuakeActivity { get; set; }
         public ObservableCollection<Model.Earthquake> EarthquakeActivity {get;} = new ObservableCollection<Model.Earthquake>();
 
         public QuakeActivityViewModel()
@@ -25,23 +26,24 @@ namespace Quakitivity.ViewModel
             //EarthquakeActivity.Add(new Model.Earthquake { Time = DateTime.Now, Magnitude = 7.2, Coordinates = new Point(12.34, 23.5) });
             //EarthquakeActivity.Add(new Model.Earthquake { Time = DateTime.Now, Magnitude = 5.2, Coordinates = new Point(132.34, 4323.5) });
             //EarthquakeActivity.Add(new Model.Earthquake { Time = DateTime.Now, Magnitude = 3.6, Coordinates = new Point(1232.34, 1123.5) });
-
+            RefreshQuakeActivity = new RelayCommand(FetchEarthquakeActivity);
             StartPeriodicTimer();
+        }
+
+        private async void FetchEarthquakeActivity(object parameter)
+        {
+            GeoJsonSummary summary = await EarthquakeUsgsGovRestAPI.GetAllHourSummary();
+            foreach (var earthquake in summary.Earthquakes)
+            {
+                EarthquakeActivity.Add(new Model.Earthquake { Time = earthquake.Time, Magnitude = earthquake.Magnitude, Coordinates = earthquake.Coordinates });
+            }
         }
 
         public void StartPeriodicTimer()
         {
-            //Start Periodic Health Check
             DispatcherTimer periodicTimer = new DispatcherTimer(DispatcherPriority.Normal);
             periodicTimer.Interval = Settings.Default.RefreshPeriod;
-            periodicTimer.Tick += new EventHandler(async (object s, EventArgs a) =>
-            {
-                GeoJsonSummary summary = await EarthquakeUsgsGovRestAPI.GetAllHourSummary();
-                foreach (var earthquake in summary.Earthquakes)
-                {
-                    EarthquakeActivity.Add(new Model.Earthquake { Time = earthquake.Time, Magnitude = earthquake.Magnitude, Coordinates = earthquake.Coordinates });
-                }
-            });
+            periodicTimer.Tick += new EventHandler((object s, EventArgs a) => { FetchEarthquakeActivity(null); });
             periodicTimer.Start();
         }
     }
