f66928e193bc44781ded7e958df6eeb6fa5dd7a9
Ralston Da Silva <ralstondasilva@gmail.com>
2016-06-18 15:43:05 -0700

    Remove unnecessary using statements

--- Quakitivity/Quakitivity/Helpers/FileHelper.cs
+++ Quakitivity/Quakitivity/Helpers/FileHelper.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using static System.IO.Compression.ZipFile;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
--- Quakitivity/Quakitivity/NetworkCommunication/EarthquakeUsgsGovRestAPI.cs
+++ Quakitivity/Quakitivity/NetworkCommunication/EarthquakeUsgsGovRestAPI.cs
@@ -1,11 +1,7 @@
 ﻿using Quakitivity.Properties;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Quakitivity.NetworkCommunication
